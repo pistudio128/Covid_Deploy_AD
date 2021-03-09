2fcc17367277b4393ed40626630e75b85f057d9f
April Dominguez <aprildominguez@Aprils-MBP.attlocal.net>
2021-03-08 21:41:19 -0500

    Requirements Fix

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,jsonify
 import pandas as pd
 import json
-from dateutil import parser
+#from dateutil import parser
 from datetime import datetime
 import numpy as np
 
